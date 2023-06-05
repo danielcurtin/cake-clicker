52253b58d38b10aefadd444263ca2b4fecf86709
Daniel Curtin <dcurtin.work@gmail.com>
2023-06-05 13:56:41 -0600

    Add local storage for save functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,18 +6,18 @@ import Cake from '../Cake/Cake';
 import Upgrades from '../Upgrades/Upgrades';
 
 const App = () => {
-  const [cakes, setCakes] = useState(0);
-  const [cps, setCps] = useState(0);
-  const [clickPowerLevel, setClickPowerLevel] = useState(1);
-  const [clickPowerCost, setClickPowerCost] = useState(100);
-  const [partyLevel, setPartyLevel] = useState(0);
-  const [partyCost, setPartyCost] = useState(5);
-  const [birthdayLevel, setBirthdayLevel] = useState(0);
-  const [birthdayCost, setBirthdayCost] = useState(50);
-  const [gradLevel, setGradLevel] = useState(0);
-  const [gradCost, setGradCost] = useState(500);
-  const [marriageLevel, setMarriageLevel] = useState(0);
-  const [marriageCost, setMarriageCost] = useState(2500);
+  const [cakes, setCakes] = useState(Number(localStorage.cakes) || 0);
+  const [cps, setCps] = useState(Number(localStorage.cps) || 0);
+  const [clickPowerLevel, setClickPowerLevel] = useState(Number(localStorage.cpl) || 1);
+  const [clickPowerCost, setClickPowerCost] = useState(Number(localStorage.cpc) || 100);
+  const [partyLevel, setPartyLevel] = useState(Number(localStorage.pl) || 0);
+  const [partyCost, setPartyCost] = useState(Number(localStorage.pc) || 5);
+  const [birthdayLevel, setBirthdayLevel] = useState(Number(localStorage.bl) || 0);
+  const [birthdayCost, setBirthdayCost] = useState(Number(localStorage.bc) || 50);
+  const [gradLevel, setGradLevel] = useState(Number(localStorage.gl) || 0);
+  const [gradCost, setGradCost] = useState(Number(localStorage.gc) || 500);
+  const [marriageLevel, setMarriageLevel] = useState(Number(localStorage.ml) || 0);
+  const [marriageCost, setMarriageCost] = useState(Number(localStorage.mc) || 2500);
   const [cpsInterval, setCpsInterval] = useState(0);
 
   useEffect(() => {
@@ -51,10 +51,22 @@ const App = () => {
   };
 
   const save = () => {
-
+    localStorage.setItem('cakes', cakes);
+    localStorage.setItem('cps', cps);
+    localStorage.setItem('cpl', clickPowerLevel);
+    localStorage.setItem('cpc', clickPowerCost);
+    localStorage.setItem('pl', partyLevel);
+    localStorage.setItem('pc', partyCost);
+    localStorage.setItem('bl', birthdayLevel);
+    localStorage.setItem('bc', birthdayCost);
+    localStorage.setItem('gl', gradLevel);
+    localStorage.setItem('gc', gradCost);
+    localStorage.setItem('ml', marriageLevel);
+    localStorage.setItem('mc', marriageCost);
   };
 
-  const restart = () => {
+  const clear = () => {
+    localStorage.clear();
     clearInterval(cpsInterval);
     setCakes(0);
     setCps(0);
@@ -74,7 +86,7 @@ const App = () => {
   return (
     <main>
       <button className='save' onClick={() => save()}>Save</button>
-      <button className='restart' onClick={() => restart()}>Restart</button>
+      <button className='clear' onClick={() => clear()}>Clear</button>
       <Points num={cakes} cps={cps}/>
       <Cake clickCake={clickCake}/>
       <Upgrades 
